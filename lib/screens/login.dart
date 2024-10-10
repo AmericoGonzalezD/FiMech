48736c8d2f9af3872fe8c042091158ef3f8a65be
Américo González <agzz2003@gmail.com>
2024-10-09 23:49:12 -0600

    Creacion de login, registro de usuarios, recuperacion

---
+++ lib/screens/login.dart
@@ -0,0 +1,296 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_validator/email_validator.dart'; // Importa el paquete email_validator para validar correos electrónicos.
+import 'package:fimech/screens/admin/homead.dart';
+import 'package:fimech/screens/recovery.dart';// Importa la pantalla de recuperación de contraseña.
+import 'package:fimech/screens/register.dart';// Importa la pantalla de registro.
+import 'package:fimech/screens/user/home.dart';// Importa la pantalla de inicio de la aplicación.
+import 'package:firebase_auth/firebase_auth.dart'; // Importa la autenticación de Firebase.
+import 'package:flutter/material.dart'; // Importa el paquete flutter material.
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    checkCurrentUser();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    checkCurrentUser();
+  }
+
+  Future<bool> _isUserAuthenticated(User user) async {
+    await user.reload();
+    return user
+        .emailVerified; // Verifica si el correo electrónico del usuario ha sido verificado
+  }
+
+  Future<void> checkCurrentUser() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null && await _isUserAuthenticated(user)) {
+      final credencial = user.uid;
+      print(credencial);
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(credencial)
+          .get();
+
+      if (userDoc.exists) {
+        final isAdmin = userDoc.data()?['isAdmin'] ?? false;
+        if (isAdmin) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePageAD()),
+            (route) =>
+                false, // Elimina todas las rutas de navegación anteriores
+          );
+        } else {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+            (route) =>
+                false, // Elimina todas las rutas de navegación anteriores
+          );
+        }
+      }
+    }
+  }
+
+  Future<void> signIn() async {
+    final isValid = _formKey.currentState!.validate();
+    if (!isValid) return;
+    try {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => const Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+
+      final User? user = userCredential.user;
+      final credencial = user?.uid;
+      if (user != null) {
+        final userDoc = await FirebaseFirestore.instance
+            .collection('admin')
+            .doc(credencial)
+            .get();
+
+        if (userDoc.exists) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePageAD()),
+            (route) =>
+                false, // Elimina todas las rutas de navegación anteriores
+          );
+        } else {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+            (route) =>
+                false, // Elimina todas las rutas de navegación anteriores
+          );
+        }
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      Navigator.pop(context);
+      if (e.code == 'wrong-password') {
+        showDialog(
+          context: context,
+          builder: (context) {
+            Future.delayed(const Duration(seconds: 1), () {
+              Navigator.of(context).pop(true);
+            });
+            return AlertDialog(
+              title: const Text('Error'),
+              content: const Text('Contraseña incorrecta. Inténtalo de nuevo.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Error al iniciar sesión, intente de nuevo')),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Accede',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 40.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'example@email.com',
+                        prefixIcon: Icon(Icons.email),
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      ),
+                      validator: (email) =>
+                          email != null && !EmailValidator.validate(email)
+                              ? 'Ingresa un correo válido'
+                              : null,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        hintText: '••••••••',
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscureText
+                              ? Icons.visibility_off
+                              : Icons.remove_red_eye),
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 12),
+                      ),
+                      obscureText: _obscureText,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) => value != null && value.length < 8
+                          ? 'Contraseña Incorrecta'
+                          : null,
+                    ),
+                  ),
+                  const SizedBox(height: 40.0),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      backgroundColor: Colors.green[400],
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Iniciar sesión'),
+                    onPressed: () async {
+                      try {
+                        await signIn();
+                      } catch (e) {
+                        if (e is FirebaseAuthException &&
+                            e.code == 'wrong-password') {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Contraseña incorrecta'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 40.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RecoverPasswordPage()),
+                      );
+                    },
+                    child: const Text(
+                      '¿Olvidaste tu contraseña?',
+                      style: TextStyle(
+                        color: Color(0xFF5DB075),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
+                      );
+                    },
+                    child: RichText(
+                      text: const TextSpan(
+                        text: '¿Aún no tienes una cuenta? ',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Regístrate',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF5DB075),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/user/profile2.dart
@@ -0,0 +1,303 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:fimech/screens/user/home.dart';
+//import 'package:fimech/screens/user/homer/widgets/profiledata.dart';
+import 'package:fimech/screens/user/widgets/circularimage.dart';
+import 'package:fimech/screens/user/widgets/profiledata.dart';
+import 'package:fimech/screens/user/widgets/sectionheading.dart';
+import 'package:fimech/screens/user/widgets/whatsappbutton.dart';
+
+class ProfilePage2 extends StatefulWidget {
+  ProfilePage2({super.key});
+
+  @override
+  _ProfilePage2State createState() => _ProfilePage2State();
+}
+
+class _ProfilePage2State extends State<ProfilePage2> {
+  String? _photoUrl;
+  Map<String, dynamic>? _userData;
+  @override
+  Widget build(BuildContext context) {
+    final User? user = FirebaseAuth.instance.currentUser;
+    final client = FirebaseFirestore.instance
+        .collection('client')
+        .doc(user?.uid)
+        .snapshots();
+    final picker = ImagePicker();
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
+        title: const Text(
+          'Perfil',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        //   actions: <Widget>[
+        //     IconButton(
+        //        icon: const Icon(Icons.notifications),
+        //       onPressed: () {
+        //          Navigator.push(
+        //           context,
+        //            MaterialPageRoute(builder: (context) => NotifiesPage()),
+        //          );
+        //        },
+        //      )
+        //     ],
+      ),
+      /*bottomNavigationBar: WhatsappButtonPerfil(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(
+              24,
+            ),
+            child: Column(
+              children: [
+                StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                  stream: client,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const CircularProgressIndicator();
+                    }
+                    // Actualiza el valor de _userData con los datos del usuario
+                    _userData = snapshot.data!.data();
+                    return Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              CircularImage(
+                                image: _userData?['image'] ??
+                                    'https://static.vecteezy.com/system/resources/previews/019/879/198/non_2x/user-icon-on-transparent-background-free-png.png',
+                                width: 140,
+                                height: 140,
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  final pickedFile = await picker.pickImage(
+                                      source: ImageSource
+                                          .gallery); // Permite al usuario seleccionar una foto de la galería
+                                  if (pickedFile != null) {
+                                    setState(() {
+                                      _photoUrl = pickedFile
+                                          .path; // Actualiza la URL de la foto seleccionada
+                                    });
+                                  }
+                                },
+                                child: const Text(
+                                  'Cambiar foto de perfil',
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const Divider(),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const SectionHeading(
+                          title: "Informacion de Usuario",
+                          showActionButton: false,
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        ProfileData(
+                          title: 'Nombre',
+                          value: _userData?['name'] ?? 'N/A',
+                          onPressed: () async {
+                            String? newName;
+                            await showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Editar nombre'),
+                                  content: TextField(
+                                    onChanged: (value) {
+                                      newName = value;
+                                    },
+                                    decoration: const InputDecoration(
+                                        hintText: 'Nombre'),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text('Cancelar'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        if (newName != _userData?['name']) {
+                                          await FirebaseFirestore.instance
+                                              .collection('client')
+                                              .doc(user?.uid)
+                                              .update({'name': newName});
+                                          setState(() {
+                                            _userData!['name'] = newName;
+                                          });
+                                        }
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Guardar'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        ProfileData(
+                            title: 'User ID',
+                            value: _userData?['uid'] ?? 'N/A',
+                            icon: Icons.copy,
+                            onPressed: () {
+                              Clipboard.setData(ClipboardData(
+                                  text: _userData?['uid'] ?? 'N/A'));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Los datos se han copiado'),
+                                ),
+                              );
+                            }),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const Divider(),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const SectionHeading(
+                          title: "Informacion Personal",
+                          showActionButton: false,
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        ProfileData(
+                            title: 'E-mail:',
+                            value: _userData?['email'] ?? 'N/A',
+                            icon: Icons.copy,
+                            onPressed: () {
+                              Clipboard.setData(ClipboardData(
+                                  text: _userData?['email'] ?? 'N/A'));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Los datos se han copiado'),
+                                ),
+                              );
+                            }),
+                        ProfileData(
+                          title: 'Telefono',
+                          value: _userData?['phone'] ?? 'N/A',
+                          onPressed: () async {
+                            String? newPhone;
+                            await showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Editar telefono'),
+                                  content: TextField(
+                                    onChanged: (value) {
+                                      newPhone = value;
+                                    },
+                                    decoration: const InputDecoration(
+                                        hintText: 'Telefono'),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text('Cancelar'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        if (newPhone != _userData?['phone']) {
+                                          await FirebaseFirestore.instance
+                                              .collection('client')
+                                              .doc(user?.uid)
+                                              .update({'phone': newPhone});
+                                          setState(() {
+                                            _userData!['phone'] = newPhone;
+                                          });
+                                        }
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Guardar'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        ProfileData(
+                          title: 'Direccion',
+                          value: _userData?['address'] ?? 'N/A',
+                          onPressed: () async {
+                            String? newAddress;
+                            await showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Editar direccion'),
+                                  content: TextField(
+                                    onChanged: (value) {
+                                      newAddress = value;
+                                    },
+                                    decoration: const InputDecoration(
+                                        hintText: 'Direccion'),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text('Cancelar'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        if (newAddress !=
+                                            _userData?['address']) {
+                                          await FirebaseFirestore.instance
+                                              .collection('client')
+                                              .doc(user?.uid)
+                                              .update({'address': newAddress});
+                                          setState(() {
+                                            _userData!['address'] = newAddress;
+                                          });
+                                        }
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Guardar'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),*/
+    );
+  }
+}
